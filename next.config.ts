4152367aaaa402b5def877b6e5d1af6fe7be0672
multiverse88 <ainanihj@gmail.com>
2026-07-07 03:48:44 +0000

    fix: backup script PG port 54322 + docker pg_dump v17; add FB/Google Ads CSP entries

--- next.config.ts
+++ next.config.ts
@@ -8,11 +8,11 @@ const ContentSecurityPolicy = isProd
   ? `
   default-src 'self';
   script-src 'self' 'unsafe-inline';
-  script-src-elem 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.youtube.com https://www.youtube-nocookie.com https://typebot.easylegal.my.id https://cdn.jsdelivr.net https://static.cloudflareinsights.com https://challenges.cloudflare.com;
+  script-src-elem 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.youtube.com https://www.youtube-nocookie.com https://typebot.easylegal.my.id https://cdn.jsdelivr.net https://static.cloudflareinsights.com https://challenges.cloudflare.com https://connect.facebook.net https://googleads.g.doubleclick.net;
   style-src 'self' 'unsafe-inline' https://typebot.easylegal.my.id;
   img-src 'self' https: blob: data:;
   font-src 'self' https://fonts.gstatic.com;
-  connect-src 'self' https://www.googletagmanager.com https://www.google-analytics.com https://sentry.io https://*.ingest.sentry.io https://typebot.easylegal.my.id https://cloudflareinsights.com;
+  connect-src 'self' https://www.googletagmanager.com https://www.google-analytics.com https://sentry.io https://*.ingest.sentry.io https://typebot.easylegal.my.id https://cloudflareinsights.com https://ad.doubleclick.net https://googleads.g.doubleclick.net https://www.googleadservices.com https://www.google.com https://connect.facebook.net https://graph.facebook.com https://www.facebook.com;
   frame-src 'self' https://www.googletagmanager.com https://www.youtube.com https://www.youtube-nocookie.com https://typebot.easylegal.my.id;
   media-src 'self' https://www.youtube.com https://www.youtube-nocookie.com blob: data:;
   object-src 'none';
