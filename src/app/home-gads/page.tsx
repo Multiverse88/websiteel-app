dea8e54046d6bffc75db4e53e0b1fcde2e767d07
Ainan <ainanihj@gmail.com>
2026-07-15 09:01:39 +0700

    fix: revert home-gads to use the original UI before homepage redesign

--- src/app/home-gads/page.tsx
+++ src/app/home-gads/page.tsx
@@ -1,11 +1,11 @@
 import { Suspense } from "react";
-import HomePage from "@/components/home/HomePage";
+import HomeGadsPage from "@/components/home/HomeGadsPage";
 import HomePageSkeleton from "@/components/home/HomePageSkeleton";
 import { LatestInsightsServer } from "@/components/home/LatestInsightsServer";
 
 async function HomeWithData() {
   const articles = await LatestInsightsServer();
-  return <HomePage articles={articles} />;
+  return <HomeGadsPage articles={articles} />;
 }
 
 export default function Page() {
