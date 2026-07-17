d0bde406f713f4e9a83cd307998ed089afdf20df
Ainan <ainanihj@gmail.com>
2026-07-17 09:44:27 +0700

    perf: change GTM script strategy to lazyOnload to improve TBT

--- src/components/GoogleTagManager.tsx
+++ src/components/GoogleTagManager.tsx
@@ -13,7 +13,7 @@ export function GtmHead() {
   return (
     <Script
       id="gtm-script"
-      strategy="afterInteractive"
+      strategy="lazyOnload"
       dangerouslySetInnerHTML={{
         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
